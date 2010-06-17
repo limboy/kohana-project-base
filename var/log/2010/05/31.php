7212fa6ce0a2f2e45577e4d82c8944c3fb17c2e6
lzyy <healdream@gmail.com>
2010-06-17 13:20:33 +0800

    add .gitignore

--- var/log/2010/05/31.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2010-05-31 11:46:48 --- ERROR: ReflectionException [ -1 ]: Class controller_hello does not exist ~ SYSPATH/classes/kohana/request.php [ 864 ]
-2010-05-31 11:47:54 --- ERROR: ReflectionException [ -1 ]: Class controller_default does not exist ~ SYSPATH/classes/kohana/request.php [ 864 ]
